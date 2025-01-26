99c2f87a2e8ca14709ed7eaad608a727bdf7c774
Filipe Andrade <filipe.engenharia42@gmail.com>
2025-01-26 18:26:49 -0300

    renamed file and new sqs resource

---
+++ deployments/sqs.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_sqs_queue" "fiap44_framer_sqs_status" {
+  name                      = "framer-status.fifo"
+  fifo_queue                  = true
+  content_based_deduplication = true
+}
+
+resource "aws_sqs_queue" "fiap44_framer_sqs_notification" {
+  name                      = "framer-notification"
+}
